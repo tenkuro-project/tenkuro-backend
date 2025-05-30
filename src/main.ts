bff7571ee00006771b31ac1be4fa9302aacda9d0
alisher134 <alisherrakhmanov134@gmail.com>
2025-05-31 01:03:15 +0500

    chore: add config for prettier, eslint and husky

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+	const port = process.env.PORT ?? 3000;
+	await app.listen(port);
 }
 bootstrap();
